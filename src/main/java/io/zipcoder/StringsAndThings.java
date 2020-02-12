839a0ecd781d7260ab153bfc4b74df7b8047b919
Han Lin <hlin@zipcodes-MacBook-Pro-6.local>
2020-02-11 20:34:07 -0500

    almost

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -56,11 +56,7 @@ public class StringsAndThings {
                 num++;
             }
         }
-        if (num % 2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return num % 2 == 0;
     }
 
     /**
